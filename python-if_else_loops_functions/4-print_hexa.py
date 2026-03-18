a10fe727f9d4a543d9e934b3b2a6bd15b880ccec
starr-5 <studying12567@gmail.com>
2026-03-18 12:04:12 +0000

    from decimal to hexadecimal

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -3,6 +3,8 @@ for i in range (0, 99):
     a=i
     result = ''
     digits = "0123456789abcdef"
+    if i == 0:
+        result = '0'
     while i > 0:
         remainder = i % 16
         result = digits[remainder] + result
