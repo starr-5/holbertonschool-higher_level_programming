128ce062dd4ae2f7a1ed4eeb4652c1bd8d9d45fa
starr-5 <studying12567@gmail.com>
2026-03-22 14:54:34 +0000

    lowercase

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def islower(c):
-    return "a" <= c <= "z"
+def uppercase(str):
+    a = ''
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            a += chr(ord(i) - 32)
+        else:
+            a += i
+    print("{}".format(a))
