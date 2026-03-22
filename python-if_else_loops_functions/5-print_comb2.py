787544d03f9e4d533489acefc0468cca4ca88b93
starr-5 <studying12567@gmail.com>
2026-03-22 07:54:37 +0000

    print numbers from 0 to 99 sep. by ','

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i <= 9:
-        num = f"0{i}"
-    else:
-        num = f"{i}"
-    
     if i == 99:
-        print(num, end="")
+        print("{:02d}".format(i), end="")
     else:
-        print(num, end=",")
+        print("{:02d}".format(i), end=",")
