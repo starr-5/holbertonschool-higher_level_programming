1ed284f359605a6d21f46f6562a75452598e3fc8
starr-5 <studying12567@gmail.com>
2026-03-22 14:48:15 +0000

    division by 3 or 5

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    a = ''
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            a += "FizzBuzz "
+        elif i % 3 == 0:
+            a += "Fizz "
+        elif i % 5 == 0:
+            a += "Buzz "
+        else:
+            a += str(i) + " "
+    print(a.strip())
