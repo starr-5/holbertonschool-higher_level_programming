d273322fae3e3f3fca6a381d95bd2ec172497d3b
starr-5 <studying12567@gmail.com>
2026-03-22 08:14:44 +0000

    print numbers from 0 to 99 if 1st is less than 2nd

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-for i in range (0, 100):
-    num1 = i//10
-    num2 = i%10
+for i in range(0, 100):
+    num1 = i // 10
+    num2 = i % 10
     if num1 < num2:
-        print("{:02d}".format(i), end=", ")
-    if i == 89:
-        print("{:02d}".format(i), end="\n")
+        if i == 89:
+            print("{:02d}".format(i), end="\n")
+        else:
+            print("{:02d}".format(i), end=", ")
