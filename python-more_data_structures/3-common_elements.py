a48b1e6fe4ce41e982073888d511fa79c1b7e1dd
starr-5 <studying12567@gmail.com>
2026-03-28 17:11:45 +0000

    set_1 & set_2 = common

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     c_set = set()
-    if i in set_1 and i in set_2:
-        c_set.append(i)
+    for i in set_1:
+        if i in set_2:
+            c_set.add(i)
     return c_set
