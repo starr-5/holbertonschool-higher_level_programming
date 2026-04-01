ef0810d093bec3195a4a43cd64bfb5db48b1ffd8
starr-5 <studying12567@gmail.com>
2026-04-01 12:41:00 +0000

    isinstance-->If size is NOT an integer, then do something

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,9 +4,11 @@
 
 class Square:
     """Defines a square."""
+
     def __init__(self, size=0):
-        """Initialize square with size."""
+        """Initialize square with optional size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
