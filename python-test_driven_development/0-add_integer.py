0bb9a486bc32c642b82f2e83dc1c0e80c8489acc
starr-5 <studying12567@gmail.com>
2026-04-02 07:54:41 +0000

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module provides a function that adds two integers."""
+
+
 def add_integer(a, b=98):
+    """Add two integers or floats and return the result as an integer."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
