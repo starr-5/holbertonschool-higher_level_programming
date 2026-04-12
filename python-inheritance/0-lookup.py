07086bee80f05a77152d012a6f56811ecf314c35
starr-5 <studying12567@gmail.com>
2026-04-12 18:07:55 +0000

    dir=what to do w that object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""Module that defines a function to list attributes and methods of an object"""
+
 def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
     return dir(obj)
