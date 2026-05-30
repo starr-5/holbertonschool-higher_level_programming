ad57b6b6c519c08153cc139d16854ed27f183666
starr-5 <studying12567@gmail.com>
2026-05-30 09:27:47 +0000

    task7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates a database and a table with primary and foreign keys
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE cities (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	state_id INT NOT NULL, 
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
