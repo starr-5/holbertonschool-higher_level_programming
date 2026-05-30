542246eeabe63129384d1584d42e9aecd43ad175
starr-5 <studying12567@gmail.com>
2026-05-30 09:26:26 +0000

    task6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates a database and atable with auto-generated id, which is also a primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       	name VARCHAR(256) NOT NULL
+);
