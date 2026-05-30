57101affd1bba068232fcff98a5914e452cc00b8
starr-5 <studying12567@gmail.com>
2026-05-30 09:19:10 +0000

    task1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creates a user with all privileges and sets a password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; 
